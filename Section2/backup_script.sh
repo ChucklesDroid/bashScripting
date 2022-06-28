ba76f6021cedb15296ae50707bfde0d5ae25faa4
ChucklesDroid <mj.akarsh@gmail.com>
2022-06-28 13:59:37 +0530

    assignment 2 section 2

---
+++ Section2/backup_script.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Author: Aakarsh MJ
+# Date Created: 26/06/22
+# Last Modified: 28/06/22 1:50PM
+
+# Description:
+# This script is used to create a backup of home_directory of the user and saves 
+# the script in form of date-month-year-time.tar in the bashScripting folder
+
+# Usage:
+# ./backup_script
+
+echo Hello, ${USER^}
+echo I will now back up your home directory, ${HOME}
+current_dir=$(pwd)
+echo You are running this script from ${current_dir}.
+echo Therefore, I will save the backup in ${current_dir}.
+
+tar -cf ${current_dir}/mybackup_$(date +%d-%m-%y_%k-%M-%S).tar ${HOME}/* 2>/dev/null
+
+echo Backup Completed Successfully
+exit 0
